3a8bf465c0e011fdae3732d464a8fd8c6c4aff30
kiteB <69155170+kiteB@users.noreply.github.com>
2021-02-12 22:31:49 +0900

    구현

---
+++ 3001~4000/3009.py
@@ -0,0 +1,36 @@
+# 세 점이 주어졌을 때, 축에 평행한 직사각형을 만들기 위해서 필요한 네 번째 점을 찾는 프로그램을 작성하시오.
+# 입력: 세 점의 좌표가 한 줄에 하나씩 주어진다. 좌표는 1보다 크거나 같고, 1000보다 작거나 같은 정수이다.
+# 출력: 직사각형의 네 번째 점의 좌표를 출력한다.
+list_x = []
+list_y = []
+
+for i in range(3):
+    x, y = map(int, input().split())
+    list_x.append(x)
+    list_y.append(y)
+
+for i in range(3):
+    if list_x.count(list_x[i]) == 1:
+        x = list_x[i]
+    if list_y.count(list_y[i]) == 1:
+        y = list_y[i]
+print(x, y)
+
+import sys
+
+x_list = []
+y_list = []
+
+for _ in range(3):
+    x, y = map(int, sys.stdin.readline().split())
+    x_list.append(x)
+    y_list.append(y)
+
+for i in range(3):
+    if x_list.count(x_list[i]) == 1:
+        x = x_list[i]
+
+    if y_list.count(y_list[i]) == 1:
+        y = x_list[i]
+
+print(x, y)
