c1d3a06fae8fad2343c64e17640d23959d359303
kiteB <mmy789@sch.ac.kr>
2021-03-27 16:02:55 +0900

    브루트포스

---
+++ 2001~3000/2529.py
@@ -0,0 +1,36 @@
+# 두 종류의 부등호 기호 '<'와 '>'가 k개 나열된 순서열 A가 있다.
+# 우리는 이 부등호 기호 앞뒤에 서로 다른 한 자릿수 숫자를 넣어서 모든 부등호 관계를 만족시키려고 한다.
+# 부등호 기호 앞뒤로 넣을 수 있는 숫자는 0 ~ 9의 정수이며 선택된 숫자는 모두 달라야 한다.
+# 제시된 k개의 부등호 순서를 만족하는 (k+1)자리의 정수 중에서 최댓값과 최솟값을 찾아야 한다.
+# 입력: 첫 줄에 부등호 문자의 개수를 나타내는 정수 k가 주어진다.
+# 그 다음 줄에는 k개의 부등호 기호가 하나의 공백을 두고 한 줄에 모두 제시된다. k의 범위는 2 <= k <= 9이다.
+# 출려기 제시된 부등호 관계를 만족하는 k+1 자리의 최대, 최소 정수를 첫째 줄과 둘째 줄에 각각 출력해야 한다.
+# 단 첫 자리가 0인 경우도 정수에 포함되어야 한다. 모든 입력에 답은 항상 존재하며 출력 정수는 하나의 문자열이 되도록 해야 한다.
+import sys
+from itertools import permutations
+
+k = int(sys.stdin.readline())
+inequality_signs = list(map(str, sys.stdin.readline().split()))
+numbers = list(range(0, 10))
+permute = permutations(numbers, k+1)
+flag = False
+answer = []
+
+for i in permute:
+    for j in range(k):
+        if inequality_signs[j] == '>':
+            if int(i[j]) > int(i[j+1]):
+                flag = True
+            else:
+                flag = False
+                break
+        elif inequality_signs[j] == '<':
+            if int(i[j]) < int(i[j+1]):
+                flag = True
+            else:
+                flag = False
+                break
+    if flag:
+        answer.append(''.join(map(str, i)))
+print(max(answer))
+print(min(answer))
