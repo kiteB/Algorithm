8a4cc5ac3aac079f939920bbc5bfbc02e8e59cc0
kiteB <mmy789@sch.ac.kr>
2021-08-01 16:59:20 +0900

    그리디

---
+++ BOJ/13001~14000/13305.py
@@ -0,0 +1,19 @@
+# 주유소
+# 각 도시에 있는 주유소의 기름 가격과, 각 도시를 연결하는 도로의 길이를 입력으로 받아 제일 왼쪽 도시에서 제일 오른쪽 도시로 이동하는 최소의 비용을 계산하는 프로그램을 작성하시오.
+# 입력: 첫 번째 줄에는 도시의 개수를 나타내는 정수 N(2 ≤ N ≤ 100,000)이 주어진다.
+# 다음 줄에는 인접한 두 도시를 연결하는 도로의 길이가 제일 왼쪽 도로부터 N-1개의 자연수로 주어진다.
+# 다음 줄에는 주유소의 리터당 가격이 제일 왼쪽 도시부터 순서대로 N개의 자연수로 주어진다.
+# 제일 왼쪽 도시부터 제일 오른쪽 도시까지의 거리는 1이상 1,000,000,000 이하의 자연수이다. 리터당 가격은 1 이상 1,000,000,000 이하의 자연수이다.
+# 표준 출력으로 제일 왼쪽 도시에서 제일 오른쪽 도시로 가는 최소 비용을 출력한다.
+import sys
+
+n = int(sys.stdin.readline())
+road = list(map(int, sys.stdin.readline().split()))
+price = list(map(int, sys.stdin.readline().split()))
+
+min_price = price[0]            # 출발할 때는 무조건 첫 번째 도시에서 주유해야함.
+answer = 0
+for i in range(n-1):
+    min_price = min(min_price, price[i])
+    answer += min_price * road[i]
+print(answer)
