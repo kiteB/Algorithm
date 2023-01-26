d1a502c6a09ed020a38551909970fb8b2b761ace
kiteB <mmy789@sch.ac.kr>
2023-01-26 11:02:26 +0900

    Modified: BOJ 1269

--- 02_백준/1000~2000/1269.py
+++ 02_백준/1000~2000/1269.py
@@ -5,4 +5,4 @@ input = sys.stdin.readline
 cnt_a, cnt_b = map(int, input().split())
 a = set(map(int, input().split()))
 b = set(map(int, input().split()))
-print(len(a ^ b))
+print(len(a ^ b))   # ^: XOR 연산자
