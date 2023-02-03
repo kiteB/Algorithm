5a3d4dcbd901038d8ca61abfcfcba8a03191d9cc
kiteB <mmy789@sch.ac.kr>
2023-02-03 18:04:12 +0900

    Solved: BOJ - 5568

--- 02_백준/5001~6000/5568.py
+++ 02_백준/5001~6000/5568.py
@@ -1,18 +1,15 @@
 # 카드 놓기
 import sys
 from itertools import permutations
-
 input = sys.stdin.readline
 
 n = int(input())
 k = int(input())
 cards = [int(input()) for _ in range(n)]
 
-numbers = []
-for p in permutations(cards, k):
-    num = ''.join(map(str, p))
+answer = set()
+# 모든 순열을 구해서 중복 없이 저장되도록 answer에 추가하기
+for p in list(permutations(cards, k)):
+    answer.add(int(''.join(map(str, p))))
 
-    if num not in numbers:
-        numbers.append(num)
-
-print(len(numbers))
+print(len(answer))
