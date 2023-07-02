066abc4c9992a58b7234f87d4f32963b92b85a0f
kiteB <mmy789@sch.ac.kr>
2023-07-03 08:02:51 +0900

    Solved: BOJ 2501

---
+++ 02_백준/2001~3000/2501.py
@@ -0,0 +1,12 @@
+# 약수 구하기 - 브론즈 3
+n, k = map(int, input().split())
+result = []
+
+for i in range(1, n + 1):
+    if n % i == 0:
+        result.append(i)
+
+if len(result) < k:
+    print(0)
+else:
+    print(result[k - 1])
