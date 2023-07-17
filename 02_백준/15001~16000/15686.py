550aafe96466bdee71a6f60b287a5412988c25dd
kiteB <mmy789@sch.ac.kr>
2023-07-17 15:45:02 +0900

    Modified: BOJ 15686

--- 02_백준/15001~16000/15686.py
+++ 02_백준/15001~16000/15686.py
@@ -1,16 +1,10 @@
 # 치킨 배달
-# 치킨 거리는 집과 가장 가까운 치킨집 사이의 거리
-# 도시의 치킨 거리는 모든 집의 치킨 거리의 합
-# 도시에 있는 치킨집 중 최대 M개를 선택했을 때, 도시의 치킨 거리의 최솟값 출력하기
-# 입력: 첫째 줄에 N과 M이 주어지며, 둘째 줄부터 N개의 줄에는 도시의 정보가 주어짐.
-# 도시의 정보는 0, 1, 2로 이루어져 있음 - 0은 빈 집, 1은 집, 2는 치킨집을 의미함.
-# 집의 개수는 2N개를 넘지 않으며, 적어도 1개는 존재한다.
-# 치킨집의 개수는 M보다 크거나 같고, 13보다 작거나 같다.
 import sys
 from itertools import combinations
+input = sys.stdin.readline
 
-n, m = map(int, sys.stdin.readline().split())
-maps = [list(map(int, sys.stdin.readline().split())) for _ in range(n)]
+n, m = map(int, input().split())
+maps = [list(map(int, input().split())) for _ in range(n)]
 houses = []             # 집의 위치를 저장할 리스트
 chicken = []            # 치킨집의 위치를 저장할 리스트
 
