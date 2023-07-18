629a9b73d281cc7d09b3a352adaef43abfe37606
kiteB <mmy789@sch.ac.kr>
2023-07-18 09:51:39 +0900

    Modified: BOJ 12904

--- 02_백준/12001~13000/12904.py
+++ 02_백준/12001~13000/12904.py
@@ -1,11 +1,11 @@
 # A와 B
 import sys
+input = sys.stdin.readline
 
-s = list(sys.stdin.readline().strip())
-t = list(sys.stdin.readline().strip())
+s = list(input().strip())
+t = list(input().strip())
 
 while len(s) != len(t):
-
     # 1. S) 문자열의 뒤에 A를 추가한다. -> T) 문자열의 뒤에 A가 있으면 제거한다.
     if t[-1] == 'A':
         t.pop()
@@ -13,7 +13,4 @@ while len(s) != len(t):
         t.pop()
         t.reverse()
 
-if s == t:
-    print(1)
-else:
-    print(0)
+print(int(s == t))
