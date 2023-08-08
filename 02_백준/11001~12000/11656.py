2787fb051630e8f88bd957c48a9b9238b45ed8ea
kiteB <mmy789@sch.ac.kr>
2023-08-08 09:05:22 +0900

    Modified: BOJ 11656

--- 02_백준/11001~12000/11656.py
+++ 02_백준/11001~12000/11656.py
@@ -1,16 +1,9 @@
-# 접미사 배열은 문자열 S의 모든 접미사를 사전순으로 정렬해 놓은 배열이다.
-# baekjoon의 접미사는 baekjoon, aekjoon, ekjoon, kjoon, joon, oon, on, n으로 총 8가지가 있고,
-# 이를 사전순으로 정렬하면, aekjoon, baekjoon, ekjoon, joon, kjoon, n, on, oon이 된다.
-# 문자열 S가 주어졌을 때, 모든 접미사를 사전순으로 정렬한 다음 출력하는 프로그램을 작성하시오.
-# 입력: 첫째 줄에 문자열 S가 주어진다. S는 알파벳 소문자로만 이루어져 있다.
-# 출력: 첫째 줄부터 S의 접미사를 사전순으로 한 줄에 하나씩 출력한다.
-import sys
+# 접미사 배열
+from sys import stdin
 
-S = str(sys.stdin.readline().strip())
-words = []
+s = stdin.readline().rstrip()
+answer = []
+for i in range(len(s)):
+    answer.append(s[i:])
 
-for i in range(len(S)):
-    words.append(S[i:])
-
-for i in sorted(words):
-    print(i)
+print(*sorted(answer), sep='\n')
