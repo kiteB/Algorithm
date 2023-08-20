b0387b38b933d2800655aa25e705ac13ba441f70
kiteB <mmy789@sch.ac.kr>
2023-08-20 14:50:01 +0900

    Modified: BOJ 11399

--- 02_백준/11001~12000/11399.py
+++ 02_백준/11001~12000/11399.py
@@ -1,12 +1,12 @@
-# ATM 앞에 N명의 사람들이 있다. 사람은 1번부터 N번까지 번호가 매겨져 있으며, i번 사람이 돈을 인출하는데 걸리는 시간은 Pi분이다.
-# 사람들이 줄을 서는 순서에 따라서, 돈을 인출하는데 필요한 시간의 합이 달라지게 된다.
-# 줄을 서 있는 사람의 수가 N과 각 사람이 돈을 인출하는데 걸리는 시간 Pi가 주어졌을 때, 각 사람이 돈을 인출하는데 필요한 시간의 합의 최솟값을 구하는 프로그램을 작성하시오.
+# ATM
 import sys
+input = sys.stdin.readline
 
-N = int(sys.stdin.readline())
-times = sorted(list(map(int, sys.stdin.readline().split())))
-sum = 0     # 시간의 합의 최솟값을 저장하기 위한 변수
+n = int(input())
+times = sorted(list(map(int, input().split())))
+answer = 0
 
-for i in range(N):
-    sum += times[i] * (N-i)     # i번째 시간은 총 (N-i)번 더해지게 된다.
-print(sum)
+for idx, val in enumerate(times):
+    answer += val * (n - idx)
+
+print(answer)
