42abb08682f21c2327a1b1fbb5fbea22e62f8812
kiteB <mmy789@sch.ac.kr>
2023-09-05 23:04:37 +0900

    Modified: BOJ 5525

--- 02_백준/5001~6000/5525.py
+++ 02_백준/5001~6000/5525.py
@@ -1,31 +1,24 @@
-# N+1개의 I와 N개의 O로 이루어져 있으면, I와 O가 교대로 나오는 문자열을 Pn이라고 한다.
-# P1: IOI
-# P2: IOIOI
-# P3: IOIOIOI
-# Pn: IOIOIO .... OI (O가 N개)
-# I와 O로만 이루어진 문자열 S와 정수 N이 주어졌을 때, S 안에 Pn이 몇 군데 포함되어 있는지 구하는 프로그램을 작성하시오.
-# 입력: 첫째 줄에 N이 주어진다. 둘째 줄에는 S의 길이 M이 주어지며, 셋째 줄에 S가 주어진다.
-# 출력: S에 Pn이 몇 군데 포함되어 있는지 출력한다.
 import sys
+input = sys.stdin.readline
 
-N = int(sys.stdin.readline())
-M = int(sys.stdin.readline())
-S = list(sys.stdin.readline().strip())
+n = int(input())
+m = int(input())
+s = input().rstrip()
 
+answer = 0
 cnt = 0
-pattern = 0
-
 i = 0
-while i < M-1:
-    if S[i-1] == 'I' and S[i] == 'O' and S[i+1] == 'I':
-        pattern += 1
 
-        if pattern == N:    # 우리가 찾고자 하는 패턴이 발견되면
-            pattern -= 1
-            cnt += 1
-        i += 1
+while i < m - 1:
+    if s[i:i + 3] == "IOI":
+        cnt += 1
+
+        if cnt == n:
+            cnt -= 1
+            answer += 1
+        i += 2
     else:
-        pattern = 0
-    i += 1
+        i += 1
+        cnt = 0
 
-print(cnt)
+print(answer)
