a9704045e407af3cafd08a45d8260dee53969d80
kiteB <mmy789@sch.ac.kr>
2023-09-07 08:31:59 +0900

    Modified: BOJ 7795

--- 02_백준/7001~8000/7795.py
+++ 02_백준/7001~8000/7795.py
@@ -1,19 +1,18 @@
 # 먹을 것인가 먹힐 것인가
 import sys
+from bisect import bisect
+input = sys.stdin.readline
 
-T = int(sys.stdin.readline())
+T = int(input())
 for _ in range(T):
-    n, m = map(int, sys.stdin.readline().split())
-    a = sorted(list(map(int, sys.stdin.readline().split())), reverse=True)
-    b = sorted(list(map(int, sys.stdin.readline().split())), reverse=True)
+    n, m = map(int, input().split())
+    a = list(map(int, input().split()))
+    b = list(map(int, input().split()))
 
-    i, j = 0, 0
-    cnt = 0
-    while i < n and j < m:
-        if a[i] > b[j]:
-            cnt += m - j
-            i += 1
-        else:
-            j += 1
+    a.sort()
+    b.sort()
 
-    print(cnt)
+    answer = 0
+    for num in a:
+        answer += bisect(b, num - 1)
+    print(answer)
