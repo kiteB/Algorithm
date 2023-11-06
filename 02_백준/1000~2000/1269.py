615e264868a996fe3dbbf819d0056287e1c9235f
kiteB <mmy789@sch.ac.kr>
2023-11-07 00:40:21 +0900

    Solved: BOJ 1269

---
+++ 02_백준/1000~2000/1269.py
@@ -0,0 +1,8 @@
+# 대칭 차집합
+import sys
+input = sys.stdin.readline
+
+cnt_a, cnt_b = map(int, input().split())
+a = set(map(int, input().split()))
+b = set(map(int, input().split()))
+print(len(a ^ b))
