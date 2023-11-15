fccfca68a465da40ae7ef4b08f00390b1924296b
kiteB <mmy789@sch.ac.kr>
2023-11-15 18:13:27 +0900

    Modified: BOJ 1138

--- 02_백준/1000~2000/1138.py
+++ 02_백준/1000~2000/1138.py
@@ -1,12 +1,12 @@
 # 한 줄로 서기
 import sys
+input = sys.stdin.readline
 
-n = int(sys.stdin.readline())
-data = list(map(int, sys.stdin.readline().split()))
+n = int(input())
+data = list(map(int, input().split()))
 answer = []
 
 for i in range(n):
     answer.insert(data[n - 1 - i], n - i)
 
-for i in answer:
-    print(i, end=' ')
+print(*answer)
