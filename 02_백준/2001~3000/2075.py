939b9e2cc7dec6a916412e468b191d734f35cf3f
kiteB <mmy789@sch.ac.kr>
2023-11-16 12:24:50 +0900

    Modified: BOJ 2075

--- 02_백준/2001~3000/2075.py
+++ 02_백준/2001~3000/2075.py
@@ -1,22 +1,18 @@
 # N번째 큰 수
-# N X N의 표에 수 N^2개 채워져 있다. 채워진 수에는 한 가지 특징이 있는데, 모든 수는 자신의 한 칸 위에 있는 수보다 크다는 것이다.
-# N번째 큰 수를 찾는 프로그램을 작성하라. 표에 채워진 수는 모두 다르다.
-# 입력: 첫째 줄에 N (1 <= N <= 1,500)이 주어진다. 다음 N개의 줄에는 각 줄마다 N개의 수가 주어진다.
-# 출력: 첫째 줄에 N번째 큰 수를 출력하라.
 import sys
 import heapq
+input = sys.stdin.readline
 
-N = int(sys.stdin.readline())
-table = list(map(int, sys.stdin.readline().split()))
+n = int(input())
+table = list(map(int, input().split()))
 heapq.heapify(table)
 
-for i in range(1, N):
-    temp = list(map(int, sys.stdin.readline().split()))
+for i in range(1, n):
+    numbers = list(map(int, input().split()))
 
-    for j in temp:
-        if table[0] < j:
-            heapq.heappush(table, j)
+    for num in numbers:
+        if table[0] < num:
+            heapq.heappush(table, num)
             heapq.heappop(table)
 
 print(table[0])
-
