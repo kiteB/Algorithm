c90d02cce8bbc0f0fa788bc5c3d90b7b72f0e4bd
kiteB <mmy789@sch.ac.kr>
2023-11-21 13:02:31 +0900

    Modified: BOJ 11004

--- 02_백준/11001~12000/11004.py
+++ 02_백준/11001~12000/11004.py
@@ -1,12 +1,9 @@
-# 수 N개 A1, A2, ..., AN이 주어진다.
-# A를 오름차순 정렬했을 때, 앞에서부터 K번째 있는 수를 구하는 프로그램을 작성하시오.
-# 입력: 첫째 줄에 N과 K가 주어진다.
-# 둘쨰 줄에는 A1, A2, ..., AN이 주어진다.
-# 출력: A를 정렬했을 때, 앞에서부터 K번째 있는 수를 출력한다.
+# K번째 수
 import sys
+input = sys.stdin.readline
 
-N, K = map(int, sys.stdin.readline().split())
-A = list(map(int, sys.stdin.readline().split()))
+n, k = map(int, input().split())
+numbers = list(map(int, input().split()))
 
-A.sort()
-print(A[K-1])
+numbers.sort()
+print(numbers[k - 1])
