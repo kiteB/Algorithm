f48cf9965c6d20742f1161ce3d9f6c222da291a0
kiteB <mmy789@sch.ac.kr>
2023-12-09 15:02:39 +0900

    Modified: BOJ 4659

--- 02_백준/4001~5000/4659.py
+++ 02_백준/4001~5000/4659.py
@@ -1,44 +1,37 @@
 # 비밀번호 발음하기
 import sys
 
+
+def is_acceptable(password):
+    vowel_flag = False
+    repeat = [0, 0]
+
+    for idx, p in enumerate(password):
+        if p in 'aeiou':
+            vowel_flag = True
+            repeat[0] += 1
+            repeat[1] = 0
+
+            if repeat[0] >= 3 or (repeat[0] >= 2 and password[idx-1] == p and p not in 'eo'):
+                return False
+
+        else:
+            repeat[1] += 1
+            repeat[0] = 0
+
+            if repeat[1] >= 3 or (repeat[1] >= 2 and password[idx-1] == p):
+                return False
+
+    return vowel_flag
+
+
 while True:
     password = sys.stdin.readline().strip()
 
-    if password == 'end':  # 'end' 입력 시 종료
+    if password == "end":
         break
 
-    vowel_flag = False  # 모음이 한 번 이상 등장했는지 판별하는 변수
-    flag = False        # 조건에 어긋나는 상황이 발생했는지를 판별하는 변수
-    repeat = [0, 0]     # 모음, 자음 반복 횟수
-
-    for idx, p in enumerate(password):
-        if p in 'aeiou':    # 모음일 경우
-            vowel_flag = True
-            repeat[0] += 1
-            repeat[1] = 0   # 자음 반복 횟수 초기화
-
-            if repeat[0] >= 3:  # 모음이 3개 이상 연속되면
-                flag = True
-                break
-
-            if repeat[0] >= 2 and password[idx - 1] == p:  # 같은 글자가 2개 이상 연속되면 안된다.
-                if p not in 'eo':   # 특수 케이스인 'ee', 'oo' 가 아닌 경우
-                    flag = True
-                    break
-
-        else:   # 자음일 경우
-            repeat[1] += 1
-            repeat[0] = 0   # 모음 반복 횟수 초기화
-
-            if repeat[1] >= 3:  # 자음이 3개 이상 연속되면
-                flag = True
-                break
-
-            if repeat[1] >= 2 and password[idx-1] == p: # 같은 글자가 2개 이상 연속되면 안된다.
-                flag = True
-                break
-
-    if not flag and vowel_flag:     # 조건에 벗어나는 경우가 없고, 모음이 1번 이상 등장했으면 acceptable
-        print(f'<{password}> is acceptable.')
+    if is_acceptable(password):
+        print(f"<{password}> is acceptable.")
     else:
-        print(f'<{password}> is not acceptable.')
+        print(f"<{password}> is not acceptable.")
