b225d140db8ac45f0c6c077b31deb2db00006034
kiteB <mmy789@sch.ac.kr>
2024-01-02 01:15:41 +0900

    Modified: BOJ 2847

--- 02_백준/2001~3000/2847.py
+++ 02_백준/2001~3000/2847.py
@@ -1,12 +1,14 @@
 # 게임을 만든 동준이
 import sys
+input = sys.stdin.readline
 
-n = int(sys.stdin.readline())
-points = [int(sys.stdin.readline()) for _ in range(n)]
+n = int(input())
+points = [int(input()) for _ in range(n)]
 
 answer = 0
 for i in range(n - 1, 0, -1):
     while points[i - 1] >= points[i]:
         points[i - 1] -= 1
         answer += 1
+
 print(answer)
