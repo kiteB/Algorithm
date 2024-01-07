7679f2e7e92aba0157234d0cb8897ed2afb08e97
kiteB <mmy789@sch.ac.kr>
2024-01-07 23:44:22 +0900

    Modified: BOJ 1747

--- 02_백준/1000~2000/1747.py
+++ 02_백준/1000~2000/1747.py
@@ -1,27 +1,26 @@
-# 어떤 수와 그 수의 숫자 순서를 뒤집은 수가 일치하는 수를 팰린드롬이라고 부른다.
-# 어떤 수 N이 주어졌을 때, N보다 크거나 같고, 소수이면서 팰린드롬인 수 중에서, 가장 작은 수를 구하시오.
-# 입력: 첫째 줄에 N이 주어진다.
-# 출력: 첫째 줄에 조건을 만족하는 수를 출력한다.
-import sys
+# 소수 & 팰린드롬
+def is_prime(num):
+    if num < 2:
+        return False
 
-N = int(sys.stdin.readline())
-max_num = 1000000           # N의 최댓값
-M = int(max_num ** 0.5)     # N의 약수의 최댓값
-prime_numbers = [False, False] + [True] * (max_num-2)   # 소수 판별 저장할 리스트
+    for i in range(2, int(num ** 0.5) + 1):
+        if num % i == 0:
+            return False
+    return True
+
+
+def is_palindrome(num):
+    return str(num) == str(num)[::-1]
+
+
+n = int(input())
+max_num = 1000000
 answer = 0
 
-# 소수 판별
-for i in range(2, M+1):
-    if prime_numbers[i]:
-        for j in range(i+i, max_num, i):
-            prime_numbers[j] = False
-
 # 팰린드롬 판별
-for i in range(N, max_num+1):
-    num = str(i)
-    reverse = num[::-1]
-    if num == reverse and prime_numbers[i]:
-        answer = num
+for i in range(n, max_num + 1):
+    if is_palindrome(i) and is_prime(i):
+        answer = i
         break
 
 # 범위 내에 답이 없으면
