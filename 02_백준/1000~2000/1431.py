96c563e02def34fb0b3ce60b32042babcb01ac09
kiteB <mmy789@sch.ac.kr>
2024-01-10 21:12:42 +0900

    Modified: BOJ

--- 02_백준/1000~2000/1431.py
+++ 02_백준/1000~2000/1431.py
@@ -1,18 +1,13 @@
 # 시리얼 번호
 import sys
+input = sys.stdin.readline
 
 
-def sum_num(numbers):   # number에 있는 총 숫자의 합을 구해주는 함수
-    result = 0
-    for i in numbers:
-        if i.isdigit():
-            result += int(i)
-    return result
+def sum_num(s):
+    return sum(int(char) for char in s if char.isdigit())
 
 
-n = int(sys.stdin.readline())
-guitars = [sys.stdin.readline().strip() for _ in range(n)]
-guitars.sort(key=lambda x: (len(x), sum_num(x), x))     # 길이, sum_num의 값 (총 숫자의 합), x 기준으로 정렬
-
-for guitar in guitars:
-    print(guitar)
+n = int(input())
+guitars = [input().strip() for _ in range(n)]
+guitars.sort(key=lambda x: (len(x), sum_num(x), x))
+print(*guitars, sep='\n')
