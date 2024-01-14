443a7eb39fa3b2ece54f5c1b85a4446ec946edb1
kiteB <mmy789@sch.ac.kr>
2024-01-14 22:28:55 +0900

    Modified: BOJ

--- 02_백준/2001~3000/2422.py
+++ 02_백준/2001~3000/2422.py
@@ -1,18 +1,19 @@
 # 한윤정이 이탈리아에 가서 아이스크림을 사먹는데
 import sys
+input = sys.stdin.readline
 
-n, m = map(int, sys.stdin.readline().split())
-combination = [[True] * (n + 1) for _ in range(n + 1)]  # 함께 먹을 수 있는지 여부 저장할 리스트
+n, m = map(int, input().split())
+combination = [[True] * (n + 1) for _ in range(n + 1)]
 
 for _ in range(m):
-    a, b = map(int, sys.stdin.readline().split())
+    a, b = map(int, input().split())
     combination[a][b] = False
     combination[b][a] = False
 
 answer = 0
 for i in range(1, n + 1):
     for j in range(i + 1, n + 1):
-        if not combination[i][j]:  # 맞지 않은 조합이면
+        if not combination[i][j]:
             continue
         for k in range(j + 1, n + 1):
             if combination[i][k] and combination[j][k]:
--- 02_백준/2001~3000/2503.py
+++ 02_백준/2001~3000/2503.py
@@ -1,30 +1,35 @@
 # 숫자 야구
 import sys
 from itertools import permutations
+input = sys.stdin.readline
 
-n = int(sys.stdin.readline())
-numbers = list(permutations(list(range(1, 10)), 3))     # 가능한 모든 경우의 수 저장
 
-tmp = []    # 제거할 리스트
-for _ in range(n):
-    test, strike, ball = map(int, sys.stdin.readline().split())     # 민혁이가 질문한 세 자리 수, 스트라이크 개수, 볼 개수
-    test = str(test)
+def bulls_and_cows(numbers, test, strike, ball):
+    remove_list = []
 
-    for num in numbers:     # 가능한 모든 경우의 수와 하나씩 비교하기
+    for num in numbers:
         s, b = 0, 0
 
         for i in range(3):
             for j in range(3):
-                if num[i] == int(test[j]):  # 숫자가 일치하고
-                    if i == j:  # 인덱스가 일치하면 스트라이크
+                if num[i] == int(test[j]):
+                    if i == j:
                         s += 1
-                    elif i != j:    # 인덱스가 다르면 볼
+                    else:
                         b += 1
 
-        if strike != s or ball != b:    # 스트라이크 수와 볼 수가 다르면 tmp 리스트에 추가
-            tmp.append(num)
+        if strike != s or ball != b:
+            remove_list.append(num)
 
-    while tmp:
-        numbers.remove(tmp.pop())   # 조건에 맞지 않는 수 모두 제거
+    for r in remove_list:
+        numbers.remove(r)
+
+
+n = int(input())
+numbers = list(permutations(list(range(1, 10)), 3))     # 가능한 모든 경우의 수 저장
+
+for _ in range(n):
+    test, strike, ball = map(int, input().split())
+    bulls_and_cows(numbers, str(test), strike, ball)
 
 print(len(numbers))
