fa14bc587bc7293f8b8d7abf51c4b54847e34ec2
kiteB <mmy789@sch.ac.kr>
2024-01-17 14:32:37 +0900

    Modified: BOJ 9081

--- 02_백준/9001~10000/9081.py
+++ 02_백준/9001~10000/9081.py
@@ -1,34 +1,28 @@
 # 단어 맞추기
 import sys
+input = sys.stdin.readline
 
 
-def next_permutation(arr):
+def find_next_permutation(arr):
     i = len(arr) - 2
 
-    # i가 0 이상이고, 뒤에 있는 값이 앞에 있는 값보다 크거나 같으면 계속 탐색
     while i >= 0 and arr[i] >= arr[i + 1]:
         i -= 1
 
-    if i == -1:  # 찾지 못하면 False 리턴
-        return False
+    if i == -1:
+        return ''.join(arr)
 
-    j = len(arr) - 1  # 뒤에서부터 탐색
-    while arr[i] >= arr[j]:  # arr[i]보다 작은 arr[j] 값을 탐색
+    j = len(arr) - 1
+    while arr[i] >= arr[j]:
         j -= 1
 
-    arr[i], arr[j] = arr[j], arr[i]  # 값 바꾸기
-    result = arr[:i + 1]  # 처음부터 i까지 result에 넣는다.
-    result.extend(list(reversed(arr[i + 1:])))  # i 이후부터는 뒤집어서 result에 넣는다.
-
-    return result
+    arr[i], arr[j] = arr[j], arr[i]
+    arr[i + 1:] = reversed(arr[i + 1:])
+    return ''.join(arr)
 
 
-T = int(sys.stdin.readline())
+T = int(input())
 for _ in range(T):
-    word = list(sys.stdin.readline().strip())
-    answer = next_permutation(word)
-
-    if not answer:  # 답을 찾지 못했으면 주어진 단어 출력
-        print(''.join(word))
-    else:
-        print(''.join(answer))
+    word = list(input().strip())
+    answer = find_next_permutation(word)
+    print(answer)
